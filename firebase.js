e689f2ed2a5d56b15491fc18e30e526f8b85cceb
niranjan-official <niranjansabarianth1521@gmail.com>
2023-06-17 16:45:47 +0530

    ui changed

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyBjf935atYEnzJvwtBU6-b-GyVcMgI05GM",
+    authDomain: "iot-switchboard-9b443.firebaseapp.com",
+    projectId: "iot-switchboard-9b443",
+    storageBucket: "iot-switchboard-9b443.appspot.com",
+    messagingSenderId: "53574015242",
+    appId: "1:53574015242:web:689b59344e2e45bd1702f3",
+    measurementId: "G-3S00EBS91R"
+};
+firebase.initializeApp(config);
+
