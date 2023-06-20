466eb48458153a3e49cc41f760647c97c2d5b9d8
niranjan-official <niranjansabarianth1521@gmail.com>
2023-06-20 22:37:04 +0530

    ui changed completely

--- script.js
+++ script.js
@@ -1,60 +1,64 @@
-
 let switches = {};
 const database = firebase.database();
 
 function initializeSwitch(switchId, switchInitialized) {
-    const switchElement = document.getElementById(switchId);
+  const switchElement = document.getElementById(switchId);
 
-    if (!switchElement) {
-        console.error(`Switch element with ID '${switchId}' not found.`);
-        return;
+  if (!switchElement) {
+    console.error(`Switch element with ID '${switchId}' not found.`);
+    return;
+  }
+
+  switches[switchId] = {
+    element: switchElement,
+    value: null,
+  };
+
+  // Listen for changes in Firebase for the specific switch
+  database.ref(`/${switchId}`).on(
+    "value",
+    (snapshot) => {
+      // Get the updated data from the snapshot
+      switches[switchId].value = snapshot.val();
+      switches[switchId].element.checked = switches[switchId].value === 1;
+      console.log(`${switchId} initial value:`, switches[switchId].value);
+      if (switchId == "switch-4") {
+        switchInitialized();
+      }
+    },
+    (error) => {
+      // Handle any errors
+      console.error(error);
     }
+  );
 
-    switches[switchId] = {
-        element: switchElement,
-        value: null
-    };
+  switches[switchId].element.addEventListener("change", () => {
+    const switchValue = switches[switchId].element.checked;
+    console.log(`Switch ${switchId} changed:`, switchValue);
+    switches[switchId].value = switchValue ? 1 : 0;
 
-    // Listen for changes in Firebase for the specific switch
-    database.ref(`/${switchId}`).on('value', (snapshot) => {
-        // Get the updated data from the snapshot
-        switches[switchId].value = snapshot.val();
-        switches[switchId].element.checked = switches[switchId].value === 1;
-        console.log(`${switchId} initial value:`, switches[switchId].value);
-        if (switchId == "switch-4") {
-            switchInitialized()
-        }
-    }, (error) => {
-        // Handle any errors
-        console.error(error);
-    });
-
-
-    switches[switchId].element.addEventListener('change', () => {
-        const switchValue = switches[switchId].element.checked;
-        console.log(`Switch ${switchId} changed:`, switchValue);
-        switches[switchId].value = switchValue ? 1 : 0;
-
-        // Update the value in Firebase for the specific switch
-        database.ref(`/${switchId}`).set(switches[switchId].value)
-            .then(() => {
-                console.log(`Switch ${switchId} value updated successfully`);
-            })
-            .catch((error) => {
-                console.error(`Error updating value for switch ${switchId}:`, error);
-            });
-    });
+    // Update the value in Firebase for the specific switch
+    database
+      .ref(`/${switchId}`)
+      .set(switches[switchId].value)
+      .then(() => {
+        console.log(`Switch ${switchId} value updated successfully`);
+      })
+      .catch((error) => {
+        console.error(`Error updating value for switch ${switchId}:`, error);
+      });
+  });
 }
 var dynamicTextElements = document.querySelectorAll(".row p");
 
 // Load stored texts from localStorage
-dynamicTextElements.forEach(function(element) {
+dynamicTextElements.forEach(function (element) {
   var storedText = localStorage.getItem(element.id);
   if (storedText) {
     element.textContent = storedText;
   }
-  
-  element.addEventListener("input", function() {
+
+  element.addEventListener("input", function () {
     var newText = element.textContent;
     localStorage.setItem(element.id, newText);
   });
